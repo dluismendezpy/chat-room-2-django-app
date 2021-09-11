67698763f4476e529c690df8c4a46fb647bf4d14
Luis Mendez <dluismendezdj@gmail.com>
2021-09-11 10:49:36 -0400

    Adding models.py

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,14 @@
+# Django
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    username = models.CharField(max_length=255)
+    room = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    class Meta:
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return f'{self.username} - {self.created_at}'
