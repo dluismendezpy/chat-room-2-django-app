89a7752bda7cf099a0651bad60b716aff80b378a
Luis Mendez <dluismendezdj@gmail.com>
2021-09-11 08:43:21 -0400

    Change custom header to bulma header

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path
 
 # Owns
-from .views import *
+from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home'),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,4 +1,5 @@
 # Django
 from django.shortcuts import render
 
-
+def home(request):
+    return render(request, 'home.html', {})
