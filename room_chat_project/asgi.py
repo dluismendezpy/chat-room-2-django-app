0a3e075a450b3ecffb6b51ac5cb10578aebf7eeb
Luis Mendez <dluismendezdj@gmail.com>
2021-09-11 11:00:27 -0400

    Configuring asgi.py

--- room_chat_project/asgi.py
+++ room_chat_project/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for room_chat_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
+# Django
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+# Owns
+from chat_app import sockets_routes
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_chat_project.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            sockets_routes.websocket_urlpatterns
+        )
+    )
+})
